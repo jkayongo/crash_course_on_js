a7b3e2671858daace19a6670124a8f23f734b058
jkayongo <jkayongo.kj@gmail.com>
2025-06-19 20:37:36 +0300

    created and worked on js maps.

--- loops.js
+++ loops.js
@@ -20,3 +20,18 @@ const course = "Data science and machine learning";
 for(let letter of course){
     console.log(letter);
 }
+//while loop
+//initialization
+//If you forget to increase the variable used in the condition, the loop will never end. This will crash your browser.
+let i = 0;
+while(i < 10){
+    console.log("The number is: " + i);
+    i++;
+}
+//do while loop-Always runs at least once — even if the condition is false.
+//Do not forget to increase the variable used in the condition, otherwise the loop will never end!
+let counter = 0;
+do{
+    console.log("The number is: " + counter);
+    counter++;
+}while(counter < 10);
---
+++ maps.js
@@ -0,0 +1,41 @@
+//A Map holds key-value pairs where the keys can be any datatype.
+//you can create a map by passing an array to new Map() or you can create a Map and use Map.set()
+//Map() expects an array of arrays, where each sub-array is a key-value pair:
+const softwareEngineers = new Map([
+    ["Kylian", "machine learning engineer"],
+    ["Vinicius", "data scientist"],
+    ["Tchoumeni", "Ai engineer"],
+    ["Courtois", "Product manager"],
+    ["Kyagulanyi", "Lead developer"]
+]);
+console.log(softwareEngineers);
+console.log("...............................")
+console.log(softwareEngineers.get("Kylian"))
+console.log("...............................")
+console.log("the number of software engineers is: " + softwareEngineers.size);
+softwareEngineers.delete("Courtois");
+console.log("...............................")
+console.log(softwareEngineers);
+//looping through a map
+softwareEngineers.forEach(function(value, key){
+    console.log("Role: " + value + " " + "Developer: " + key);
+})
+//looping through keys
+for(const key of softwareEngineers.keys()){
+    console.log("The company developer is: " + key);
+}
+//looping through values
+for(const value of softwareEngineers.values()){
+    console.log("Profession: " + value);
+}
+//creating a map and adding elements using the set() Method
+const scienceStudents = new Map();
+scienceStudents.set("Biology", 90);
+scienceStudents.set("Physics", 67);
+scienceStudents.set("Mathematics", 200);
+scienceStudents.set("Chemistry", 70);
+console.log(scienceStudents);
+//the set() method can also be used to change existing Map values:
+scienceStudents.set("Chemistry", 95);
+console.log(scienceStudents);
+
